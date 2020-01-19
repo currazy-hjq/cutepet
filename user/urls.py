24940a848153ce465f78059996e5669eacd31e1f
hjq <a1006923031@outlook.com>
2020-01-19 12:07:31 +0800

    完善邮箱激活流程

--- user/urls.py
+++ user/urls.py
@@ -5,4 +5,6 @@ from user import views
 urlpatterns = [
     # http://127.0.0.1:8000/v1/users/
     url(r'^$', views.Users.as_view()),
+    # http://127.0.0.1:8000/v1/users/activation?code=xxxxx
+    url(r'^activation$',views.user_active),
 ]
--- user/views.py
+++ user/views.py
@@ -56,8 +56,9 @@ class Users(View):
 
         # 发送激活邮件
         random_int = random.randint(1000, 9999)
-        code_str = username + str(random_int)
+        code_str = username + '_' + str(random_int)
         code_str_bs = base64.urlsafe_b64encode(code_str.encode())
+
 
         # 将随机码储存在redis中,过期时间两天
         # TODO 上线后增加密码  为什么存redis
@@ -66,8 +67,45 @@ class Users(View):
         # TODO 上线后修改地址
         active_url = TESTURL + '/cutepet/templates/active.html?code=%s' % code_str_bs.decode()
 
-        #异步发邮件
+        # 异步发邮件
         send_active_email.delay(email, active_url)
         result = {'code': 200, 'username': username, 'data': {'token': token.decode()}}
         return JsonResponse(result)
 
+
+def user_active(request):
+    if request.method != 'GET':
+        return JsonResponse({'code': 10100, 'error': 'Please use GET'})
+
+    r = redis.Redis(host='127.0.0.1', port='6379', db=1)
+    # http://127.0.0.1:8000/v1/users/activation?code=MTIzOTk5Xzk4Nzk=
+
+    # 解析code
+    code = request.GET.get('code')
+    if not code:
+        result = {'code': '10105', 'error': {'message': 'Code not found'}}
+        return JsonResponse(result)
+    try:
+        code_str = base64.urlsafe_b64decode(code.encode())
+        new_code = code_str.decode()  # username + randint
+        username, rcode = new_code.split('_')
+    except:
+        return JsonResponse({'code': 10103, 'error': 'Your code is wrong!'})
+
+    # 从redis中验证username和randint是否匹配
+    old_code = r.get('email_active_%s' % username)
+    if old_code != code.encode():
+        return JsonResponse({'code': 10104, 'error': 'Your code is wrong!'})
+
+    # 激活用户
+    user = User.objects.filter(username=username)
+    if not user:
+        return JsonResponse({'code': 10105, 'error': "User isn't exist"})
+    user = user[0]
+    user.isactive = True
+    user.save()
+
+    # 删除redis
+    r.delete('email_active_%s' % username)
+
+    return JsonResponse({'code': 200, 'status': '激活成功'})
